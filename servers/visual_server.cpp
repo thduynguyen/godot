3eb2d03a722edebc94e3d3a38403e1523e948177
Duy-Nguyen Ta <thduynguyen@gmail.com>
2017-12-12 11:16:08 -0500

    silent casting errors when compiling inside drake

--- servers/visual_server.cpp
+++ servers/visual_server.cpp
@@ -440,9 +440,9 @@ Error VisualServer::_surface_set_data(Array p_arrays, uint32_t p_format, uint32_
 					for (int i = 0; i < p_vertex_array_len; i++) {
 
 						int8_t vector[4] = {
-							CLAMP(src[i].x * 127, -128, 127),
-							CLAMP(src[i].y * 127, -128, 127),
-							CLAMP(src[i].z * 127, -128, 127),
+							static_cast<int8_t>(CLAMP(src[i].x * 127, -128, 127)),
+							static_cast<int8_t>(CLAMP(src[i].y * 127, -128, 127)),
+							static_cast<int8_t>(CLAMP(src[i].z * 127, -128, 127)),
 							0,
 						};
 
@@ -475,10 +475,10 @@ Error VisualServer::_surface_set_data(Array p_arrays, uint32_t p_format, uint32_
 					for (int i = 0; i < p_vertex_array_len; i++) {
 
 						uint8_t xyzw[4] = {
-							CLAMP(src[i * 4 + 0] * 127, -128, 127),
-							CLAMP(src[i * 4 + 1] * 127, -128, 127),
-							CLAMP(src[i * 4 + 2] * 127, -128, 127),
-							CLAMP(src[i * 4 + 3] * 127, -128, 127)
+							static_cast<uint8_t>(CLAMP(src[i * 4 + 0] * 127, -128, 127)),
+							static_cast<uint8_t>(CLAMP(src[i * 4 + 1] * 127, -128, 127)),
+							static_cast<uint8_t>(CLAMP(src[i * 4 + 2] * 127, -128, 127)),
+							static_cast<uint8_t>(CLAMP(src[i * 4 + 3] * 127, -128, 127))
 						};
 
 						copymem(&vw[p_offsets[ai] + i * p_stride], xyzw, 4);
@@ -711,7 +711,7 @@ Error VisualServer::_surface_set_data(Array p_arrays, uint32_t p_format, uint32_
 
 		if (first) {
 			for (int i = 0; i < total_bones; i++) {
-				r_bone_aabb[i].size == Vector3(-1, -1, -1); //negative means unused
+				r_bone_aabb[i].size = Vector3(-1, -1, -1); //negative means unused
 			}
 		}
 
